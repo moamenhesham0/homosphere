8f39811c99c6c8943d10fc16d293ed9ce9275895
AhmedFaragElsayed <153814689+AhmedFaragElsayed@users.noreply.github.com>
2025-11-27 09:28:52 +0200

    handle upgrade and downgrade subscription
    edit colors of profile and handle upgrade and downgrade subscription

--- homosphere-frontend/src/main.jsx
+++ homosphere-frontend/src/main.jsx
@@ -3,15 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
-import { ROUTES } from '@constants/routes.js';
-import SignUp from '@pages/signUpPage.jsx';
-import SignIn from '@pages/signInPage.jsx';
-import ForgetPassword from '@pages/forgetPasswordPage';
-import AuthPage from '@pages/authPage.jsx';
 import SignUp from './pages/signUpPage.jsx';
 import SignIn from './pages/signInPage.jsx';
-import Layout from './components/Layout.jsx';
-import { AuthProvider } from './contexts/AuthContext.jsx';
 import SubscriptionPage from './pages/SubscriptionPage.jsx';
 import Profile from './pages/Profile.jsx';
 
@@ -19,11 +12,6 @@ createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route path={ROUTES.HOME} element={<App />} />
-                <Route path={ROUTES.SIGNUP} element={<SignUp />} />
-                <Route path={ROUTES.SIGNIN} element={<SignIn />} />
-                <Route path={ROUTES.FORGET_PASSWORD} element={<ForgetPassword />} />
-                <Route path={ROUTES.AUTH} element={<AuthPage />} />
                 <Route path="/" element={<App />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/signin" element={<SignIn />} />
